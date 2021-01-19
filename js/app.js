90b09d8f88faf2da57e19695acb6ec4fe98d6522
Nikitas Kastis <nikitaskastis15@gmail.com>
2021-01-19 15:55:14 +0200

    correcting null choice in radio

--- js/app.js
+++ js/app.js
@@ -65,42 +65,52 @@ function showSuccessOrNotD(event) {
 
     let button = event.target;
     let answers = button.parentElement;
-    let answer = answers.querySelector('div label input[type="radio"]:checked').value;
-    let question = answers.querySelector('div p');
-    // console.log(answer);
-    // console.log(answers);
-    if (answer === "error") {
-        //if the user had selected right and then selected wrong
-        if (answers.style.color === "green") {
-            total = total - 2;
-        }
-        // if the user selected wrong for a second time, do nothing
-        else if (answers.style.color === "red") {
-        }
-        // if user's first selected answer is wrong
-        else {
-            total = total - 1;
-        }
-        answers.style.color = "red";
-        answers.style.borderColor = "red";
-        result.innerText = total;
-    }
-    else if (answer === "correct") {
-        //if the user had selected wrong and then selected right
-        if (answers.style.color === "red") {
-            total = total + 2;
-        }
-        // if user's first selected answer is right
-        else {
-            total = total + 1;
-        }
-        answers.style.color = "green";
-        answers.style.borderColor = "green";
-        result.innerText = total;
+    if (answers.querySelector('div label input[type="radio"]:checked') !== null) {
+        let answer = answers.querySelector('div label input[type="radio"]:checked').value;
+        let question = answers.querySelector('div p');
+        // console.log(answer);
+        // console.log(answers);
 
+        if (answer === "error") {
+            //if the user had selected right and then selected wrong
+            if (answers.style.color === "green") {
+                total = total - 2;
+            }
+            // if the user selected wrong for a second time, do nothing
+            else if (answers.style.color === "red") {
+            }
+            // if user's first selected answer is wrong
+            else {
+                total = total - 1;
+            }
+            answers.style.color = "red";
+            answers.style.borderColor = "red";
+            result.innerText = total;
+        }
+        else if (answer === "correct") {
+            //if the user had selected wrong and then selected right
+            if (answers.style.color === "red") {
+                total = total + 2;
+            }
+            // if user's first selected answer is right
+            else {
+                total = total + 1;
+            }
+            answers.style.color = "green";
+            answers.style.borderColor = "green";
+            result.innerText = total;
+
+        }
+
+        question.style.color = "white";
+    }
+    else {
+        button.innerHTML = "You have not answered this question.";
+        setTimeout(function () {
+            button.innerHTML = "Submit Answer"
+        }, 2000)
     }
 
-    question.style.color = "white";
 }
 
 
